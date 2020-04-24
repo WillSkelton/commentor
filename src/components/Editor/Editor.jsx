31931f9f5b10c5325768295ae34d27e8b4a8777d
WillSkelton <william.f.skelton@gmail.com>
2020-04-23 18:02:41 -0700

    we have code

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { makeStyles, TextField } from "@material-ui/core";
 import { colors } from "../../services";
+import Highlight from "react-highlight.js";
 
 const useStyles = makeStyles({
   root: {
@@ -14,34 +15,64 @@ const useStyles = makeStyles({
   textField: {},
   textFieldBigInput: {},
   textFieldLittleInput: {
-    border: `2px solid ${colors.blue}`,
+    border: `2px solid ${colors.slate}`,
     borderRadius: "4px",
   },
-  textFieldInputLabel: {
-    color: colors.blue,
-    backgroundColor: colors.backgroundGray,
-  },
 });
+
+const code = `
+package api
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	port         = "42201"
+	tempHomePage = \`
+	<h1>Welcome to the Commentor Homepage</h1>
+	<p>There isn't much here now but there will be soon.</p>
+	\`
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint hit: /homepage")
+	fmt.Fprintf(w, tempHomePage)
+}
+
+func handleRequests() {
+	// This tells the server to run the function homePage when a request is sent to
+	// "/" which is the home page
+	http.HandleFunc("/", homePage)
+
+	fmt.Println("Listening on port: 42201")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+// Start will setup the routes and their respective functions as well as telling the
+// server which port to listen on
+func Start() {
+	handleRequests()
+}
+`;
 
 const Editor = () => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <TextField
+      {/* <TextField
         className={classes.textField}
         variant='outlined'
         color='secondary'
-        label='Yeet'
-        InputLabelProps={{
-          className: classes.textFieldInputLabel,
-          shrink: "true",
-        }}
         InputProps={{}}
         inputProps={{
           className: classes.textFieldLittleInput,
         }}
-      ></TextField>
+      ></TextField> */}
+      <Highlight language={"go"}>{code}</Highlight>
     </div>
   );
 };
